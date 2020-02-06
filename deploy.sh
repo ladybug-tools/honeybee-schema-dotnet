924dc16a02ac4c1c97e56bb936c9731f08cec0d4
MingboPeng <mingo1214@gmail.com>
2020-02-05 21:21:11 -0500

    fix(NuGet): fixed nupkg name.

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-
-nuget pack ./src/HoneybeeDotNet/HoneybeeDotNet.csproj -Build -Verbosity detailed
-
-nuget push ./HoneybeeDotNet.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source https://api.nuget.org/v3/index.json
