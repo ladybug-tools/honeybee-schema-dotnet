37b5fad11fd41d479b656e8edff7861d6b573249
MingboPeng <mingo1214@gmail.com>
2020-02-07 19:27:33 -0500

    fix(Deploy): NuGet Deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 nuget pack src/HoneybeeDotNet/HoneybeeDotNet.csproj -Properties Configuration=Release -Version $BUILD_VERSION -Verbosity detailed && \
-nuget push HoneybeeDotNet.$BUILD_VERSION.nupkg -ApiKey $NUGET_API_KEY -Source nuget.org -Verbosity detailed
+nuget push HoneybeeDotNet.$BUILD_VERSION.nupkg -ApiKey $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json
