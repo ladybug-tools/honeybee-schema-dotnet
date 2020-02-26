6152929f6d4f32f932e9e229803bddc2aa8a3986
AntoineDao <antoinedao1@gmail.com>
2020-02-26 11:13:41 +1000

    build(open-api): change generator scripts to build from github release instead of github pages
    This provides a more deterministic way to generate clients based on release versions rather than
    what the latest schema displayed on github pages is.
    
    #38

--- .openapi-generator/generate.sh
+++ .openapi-generator/generate.sh
@@ -1,6 +1,14 @@
+if [ -z "$1" ]
+  then
+    echo "Schema release version must be provided"
+    echo "eg: bash .openapi-generator/generate.sh v1.17.0"
+fi
+
+export VERSION=$1
+
 python .openapi-generator/pre_gen_script.py
-npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/simulation-parameter.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double 
-npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/model.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double
+npx openapi-generator generate -i "https://github.com/ladybug-tools/honeybee-schema/releases/download/$VERSION/simulation-parameter.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double 
+npx openapi-generator generate -i "https://github.com/ladybug-tools/honeybee-schema/releases/download/$VERSION/model.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double
 python .openapi-generator/post_gen_script.py
 python .openapi-generator/update_assembly_version.py
 python .openapi-generator/update_type_with_interface.py
