0f997c9af87a20191487b0e4b911b1c3327d48da
ladybugbot <ladybugbot@users.noreply.github.com>
2020-03-27 21:24:17 +0000

    Update Schema to v1.18.0

--- src/HoneybeeSchema/BaseClasses/NamedReferenceable.cs
+++ src/HoneybeeSchema/BaseClasses/NamedReferenceable.cs
@@ -1,18 +1,18 @@
 namespace HoneybeeSchema
 {
-public partial class AirBoundaryConstructionAbridged: INamed {}
-public partial class ElectricEquipmentAbridged: INamed {}
-public partial class GasEquipmentAbridged: INamed {}
-public partial class IdealAirSystemAbridged: INamed {}
-public partial class InfiltrationAbridged: INamed {}
-public partial class LightingAbridged: INamed {}
-public partial class OpaqueConstructionAbridged: INamed {}
 public partial class PeopleAbridged: INamed {}
+public partial class LightingAbridged: INamed {}
+public partial class WindowConstructionAbridged: INamed {}
+public partial class GasEquipmentAbridged: INamed {}
+public partial class OpaqueConstructionAbridged: INamed {}
+public partial class ScheduleRulesetAbridged: INamed {}
+public partial class IdealAirSystemAbridged: INamed {}
 public partial class ProgramTypeAbridged: INamed {}
 public partial class ScheduleFixedIntervalAbridged: INamed {}
-public partial class ScheduleRulesetAbridged: INamed {}
 public partial class SetpointAbridged: INamed {}
+public partial class InfiltrationAbridged: INamed {}
 public partial class VentilationAbridged: INamed {}
-public partial class WindowConstructionAbridged: INamed {}
+public partial class AirBoundaryConstructionAbridged: INamed {}
+public partial class ElectricEquipmentAbridged: INamed {}
 public partial class ConstructionSetAbridged: INamed{}
 }
