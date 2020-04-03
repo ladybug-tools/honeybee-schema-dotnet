dc8695833a126376c20f688722e4d226d7b08929
ladybugbot <ladybugbot@users.noreply.github.com>
2020-04-03 21:23:44 +0000

    Update Schema to v1.19.0

--- src/HoneybeeSchema/BaseClasses/EnergyWindowMaterial.cs
+++ src/HoneybeeSchema/BaseClasses/EnergyWindowMaterial.cs
@@ -1,10 +1,10 @@
 namespace HoneybeeSchema
 {
-public partial class EnergyWindowMaterialGlazing: IEnergyWindowMaterial {}
-public partial class EnergyWindowMaterialSimpleGlazSys: IEnergyWindowMaterial {}
-public partial class EnergyWindowMaterialGasMixture: IEnergyWindowMaterial {}
-public partial class EnergyWindowMaterialBlind: IEnergyWindowMaterial {}
-public partial class EnergyWindowMaterialGas: IEnergyWindowMaterial {}
-public partial class EnergyWindowMaterialShade: IEnergyWindowMaterial {}
 public partial class EnergyWindowMaterialGasCustom: IEnergyWindowMaterial {}
+public partial class EnergyWindowMaterialGlazing: IEnergyWindowMaterial {}
+public partial class EnergyWindowMaterialBlind: IEnergyWindowMaterial {}
+public partial class EnergyWindowMaterialGasMixture: IEnergyWindowMaterial {}
+public partial class EnergyWindowMaterialShade: IEnergyWindowMaterial {}
+public partial class EnergyWindowMaterialSimpleGlazSys: IEnergyWindowMaterial {}
+public partial class EnergyWindowMaterialGas: IEnergyWindowMaterial {}
 }
