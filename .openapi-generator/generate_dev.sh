26013501a5de9e7409b8bb6c1eb36d48ff8dad7a
MingboPeng <mingo1214@gmail.com>
2020-04-07 17:41:57 -0400

    chore(dev): add local development

---
+++ .openapi-generator/generate_dev.sh
@@ -0,0 +1,8 @@
+
+python .openapi-generator/pre_gen_script.py
+npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/simulation-parameter.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double 
+npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/model.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double 
+python .openapi-generator/post_gen_script.py "https://www.ladybug.tools/honeybee-schema/simulation-parameter.json"
+python .openapi-generator/post_gen_script.py "https://www.ladybug.tools/honeybee-schema/model.json"
+python .openapi-generator/update_assembly_version.py
+python .openapi-generator/update_type_with_interface.py
