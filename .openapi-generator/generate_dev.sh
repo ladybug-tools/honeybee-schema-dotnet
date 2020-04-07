c6c4b716dbd36c044b6218fb1faf42cad79596a0
MingboPeng <mingo1214@gmail.com>
2020-04-07 17:41:57 -0400

    chore(dev): local development

--- .openapi-generator/generate_dev.sh
+++ .openapi-generator/generate_dev.sh
@@ -1,5 +1,5 @@
 
-python .openapi-generator/pre_gen_script.py
+python .openapi-generator/pre_gen_script.py "v1.20.0"
 npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/simulation-parameter.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double 
 npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/model.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double 
 python .openapi-generator/post_gen_script.py "https://www.ladybug.tools/honeybee-schema/simulation-parameter.json"
