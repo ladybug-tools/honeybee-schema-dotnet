7f8de3949ad18418adda22bac68bdf719f8664ea
MingboPeng <mingo1214@gmail.com>
2020-04-07 20:37:35 -0400

    fix(dev): pre_gen_script workflow for local dev

--- .openapi-generator/pre_gen_script.py
+++ .openapi-generator/pre_gen_script.py
@@ -1,13 +1,25 @@
-import os, sys
-import urllib.request, json
+import os
+import sys
+import urllib.request
+import json
 
-version = sys.argv[1:][0]
 
-version = version.replace('v', '')
+args = sys.argv[1:]
+version = ""
+
+if args == []:
+    source_json_url = "https://www.ladybug.tools/honeybee-schema/model.json"
+    json_url = urllib.request.urlopen(source_json_url)
+    data = json.loads(json_url.read())
+    version = data['info']['version'][0]
+else:
+    version = args[0]
 
 print(version)
+version = version.replace('v', '')
 
-config_file = os.path.join(os.getcwd(),'.openapi-generator','config.json')
+
+config_file = os.path.join(os.getcwd(), '.openapi-generator', 'config.json')
 
 with open(config_file, "r") as jsonFile:
     config_data = json.load(jsonFile)
@@ -16,3 +28,5 @@ config_data["packageVersion"] = version
 
 with open(config_file, "w") as jsonFile:
     json.dump(config_data, jsonFile, indent=4)
+
+
