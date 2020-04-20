d96f31eb7291211e25a7645099313992d880af6a
MingboPeng <mingo1214@gmail.com>
2020-04-20 15:27:29 -0400

    fix(generator): fix version, add fix_constructor(), clean up all *AllOf.cs files

--- .openapi-generator/generate_dev.sh
+++ .openapi-generator/generate_dev.sh
@@ -5,19 +5,6 @@ npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/mod
 python .openapi-generator/post_gen_script.py "https://www.ladybug.tools/honeybee-schema/simulation-parameter_inheritance.json"
 python .openapi-generator/post_gen_script.py "https://www.ladybug.tools/honeybee-schema/model_inheritance.json"
 python .openapi-generator/update_assembly_version.py
+python .openapi-generator/create_interface.py "https://www.ladybug.tools/honeybee-schema/simulation-parameter_mapper.json"
 python .openapi-generator/create_interface.py "https://www.ladybug.tools/honeybee-schema/model_mapper.json"
 
-
-# npx openapi-generator generate -i "test.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json --type-mappings decimal=double
-# java -jar .openapi-generator/openapi-generator.jar generate -i "test.json" -g csharp -t ".openapi-generator/templates/csharp" -o . --skip-validate-spec -c .openapi-generator/config.json
-# java -jar .openapi-generator/swagger-codegen-cli.jar generate -i "test.json" -l csharp -t ".openapi-generator/templates/csharp" -o "temp" -c .openapi-generator/config.json --verbose
-
-# java -jar .openapi-generator/openapi-generator.jar generate -i "test.json" -g csharp -o temp_apiGen --skip-validate-spec
-
-# java -jar .openapi-generator/swagger-codegen-cli.jar generate -i "test.json" -l csharp --additional-properties targetFramework=v4.5 -o temp/
-# java -jar .openapi-generator/swagger-codegen-cli.jar generate -i https://gist.githubusercontent.com/danielflower/5c5ae8a46a0a49aee508690c19b33ada/raw/b06ff4d9764b5800424f6a21a40158c35277ee65/petstore.json -l csharp --additional-properties targetFramework=v4.5 -o temp/
-
-# npx openapi-generator generate -i "model.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json
-# python .openapi-generator/post_gen_script.py "model.json"
-# python .openapi-generator/update_assembly_version.py
-# python .openapi-generator/create_interface.py "https://www.ladybug.tools/honeybee-schema/model_mapper.json"
--- .openapi-generator/update_assembly_version.py
+++ .openapi-generator/update_assembly_version.py
@@ -1,16 +1,17 @@
 import re, os, json
 
-config_file = os.path.join(os.getcwd(),'.openapi-generator','config.json')
+config_file = os.path.join(os.getcwd(), '.openapi-generator', 'config.json')
 
 with open(config_file, "r") as jsonFile:
     config_data = json.load(jsonFile)
 
 schema_version = config_data["packageVersion"]
 
-assembly_file = os.path.join(os.getcwd(),'src', 'HoneybeeSchema', 'Properties','AssemblyInfo.cs')
+assembly_file = os.path.join(os.getcwd(), 'src', 'HoneybeeSchema', 'Properties', 'AssemblyInfo.cs')
 
 with open(assembly_file, "r") as csFile:
     s = csFile.read()
 with open(assembly_file, 'w') as f:
-    s = re.sub("\d+\.?\d+\.?\d+\.?\*", schema_version + '.*' , s)
+    s = re.sub(r"(?<=assembly: AssemblyVersion\(\")\S+(?=\"\))", schema_version + '.*', s)
+    print(f"Update AssemblyVersion to: {schema_version}")
     f.write(s)
