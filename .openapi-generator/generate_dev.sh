db1a796adb73217c803b4e3fbad3ea61a15941c0
MingboPeng <mingo1214@gmail.com>
2020-04-28 17:40:17 -0400

    fix(generator): generate_dev with local json files

--- .openapi-generator/generate_dev.sh
+++ .openapi-generator/generate_dev.sh
@@ -8,3 +8,11 @@ python .openapi-generator/update_assembly_version.py
 python .openapi-generator/create_interface.py "https://www.ladybug.tools/honeybee-schema/simulation-parameter_mapper.json"
 python .openapi-generator/create_interface.py "https://www.ladybug.tools/honeybee-schema/model_mapper.json"
 
+
+npx openapi-generator generate -i ".openapi-docs/simulation-parameter_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json  
+npx openapi-generator generate -i ".openapi-docs/model_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
+python .openapi-generator/post_gen_script.py ".openapi-docs/simulation-parameter_inheritance.json"
+python .openapi-generator/post_gen_script.py ".openapi-docs/model_inheritance.json"
+python .openapi-generator/update_assembly_version.py
+python .openapi-generator/create_interface.py ".openapi-docs/simulation-parameter_mapper.json"
+python .openapi-generator/create_interface.py ".openapi-docs/model_mapper.json"
