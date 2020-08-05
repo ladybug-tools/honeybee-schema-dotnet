7fd668391889fc1c062782f6f303c776fab2a912
MingboPeng <mingo1214@gmail.com>
2020-08-05 15:54:55 -0400

    fix(): fix CD with new dotnet

--- .openapi-generator/update_assembly_version.py
+++ .openapi-generator/update_assembly_version.py
@@ -1,17 +1,53 @@
-import re, os, json
+import re
+import os
+import json
+import urllib.request
 
+package_name = 'HoneybeeSchema'
+
+# Check the version from config
 config_file = os.path.join(os.getcwd(), '.openapi-generator', 'config.json')
-
 with open(config_file, "r") as jsonFile:
     config_data = json.load(jsonFile)
 
 schema_version = config_data["packageVersion"]
+print(f'Schema_version: {schema_version}')
+new_version = f'{schema_version}.0'
 
-assembly_file = os.path.join(os.getcwd(), 'src', 'HoneybeeSchema', 'Properties', 'AssemblyInfo.cs')
+# Check the version from nuget
+api = f'https://api.nuget.org/v3-flatcontainer/{package_name}/index.json'
+with urllib.request.urlopen(api) as r:
+    data = json.loads(r.read())
+    versions = [v for v in data['versions'] if v.startswith(schema_version)]
+    if versions != []:
+        versions.sort(reverse=True)
+        new_version = versions[0]
+        print(f'Found latest version on Nuget: {new_version}')
 
-with open(assembly_file, "r") as csFile:
+
+# increment version
+version_digits = new_version.split('.')
+
+
+print(len(version_digits))
+if len(version_digits) == 3:
+    new_version = f'{new_version}.1'
+elif version_digits[-1] != '0':
+    v = int(version_digits[-1]) + 1
+    version_digits = version_digits[0:-1]
+    version_digits.append(str(v))
+    new_version = '.'.join(version_digits)
+
+
+
+print(f'New version: {new_version}')
+
+# update the version
+assembly_file = os.path.join(os.getcwd(), 'src', package_name, f'{package_name}.csproj')
+
+with open(assembly_file, encoding='utf-8', mode='r') as csFile:
     s = csFile.read()
-with open(assembly_file, 'w') as f:
-    s = re.sub(r"(?<=assembly: AssemblyVersion\(\")\S+(?=\"\))", schema_version + '.*', s)
-    print(f"Update AssemblyVersion to: {schema_version}")
+with open(assembly_file, encoding='utf-8', mode='w') as f:
+    s = re.sub(r"(?<=\SVersion\>)\S+(?=\<\/)", new_version, s)
+    print(f"Update AssemblyVersion to: {new_version}")
     f.write(s)
