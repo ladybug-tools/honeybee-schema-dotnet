ed617005d1b179a72d5878e02f451d9f7d419b90
MingboPeng <mingo1214@gmail.com>
2020-08-05 15:54:55 -0400

    fix(): fix CD with new dotnet

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-nuget pack src/HoneybeeSchema/HoneybeeSchema.csproj -Properties Configuration=Release -Version $BUILD_VERSION -Verbosity detailed && \
-nuget push HoneybeeSchema.$BUILD_VERSION.nupkg -ApiKey $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json
