633185abdaedde5151e57fe5c3ac02b9e1b43016
MingboPeng <mingo1214@gmail.com>
2020-09-03 15:50:16 -0400

    perf(ModelProperties): add ModelProperties.Default

---
+++ src/HoneybeeSchema/Partial/ModelProperties.cs
@@ -0,0 +1,25 @@
+﻿
+namespace HoneybeeSchema
+{
+    partial class ModelProperties
+    {
+		private static ModelProperties _default;
+
+		public static ModelProperties Default
+		{
+			get 
+			{
+				if (_default == null)
+				{
+					_default = new ModelProperties(
+						Helper.EnergyLibrary.DefaultModelEnergyProperties, 
+						Helper.EnergyLibrary.DefaultModelRadianceProperties
+						);
+				}
+
+				return _default; 
+			}
+		}
+
+	}
+}
