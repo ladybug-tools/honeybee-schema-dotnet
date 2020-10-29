96d3b0b73949edb9b98ef821384700a6df526d87
MingboPeng <mingo1214@gmail.com>
2020-10-29 17:53:58 -0400

    chore(gen): fix generate.sh with openapi-generator-cli

--- .openapi-generator/generate.sh
+++ .openapi-generator/generate.sh
@@ -9,8 +9,8 @@ export VERSION=$1
 python .openapi-generator/downloadHoneybeeSchema.py "https://github.com/ladybug-tools/honeybee-schema/releases/download/$VERSION"
 python .openapi-generator/pre_gen_script.py ".openapi-docs/model_inheritance.json"
 
-npx openapi-generator generate -i ".openapi-docs/simulation-parameter_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json  
-npx openapi-generator generate -i ".openapi-docs/model_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
+npx @openapitools/openapi-generator-cli generate -i ".openapi-docs/simulation-parameter_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json  
+npx @openapitools/openapi-generator-cli generate -i ".openapi-docs/model_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
 
 python .openapi-generator/post_gen_script.py ".openapi-docs/simulation-parameter_inheritance.json"
 python .openapi-generator/post_gen_script.py ".openapi-docs/model_inheritance.json"
--- .openapi-generator/generate_dev.sh
+++ .openapi-generator/generate_dev.sh
@@ -2,11 +2,15 @@
 # Install openapi-generator
 #   npm install @openapitools/openapi-generator-cli@cli-4.3.0
 
+# check version
+# npx @openapitools/openapi-generator-cli version-manager list
+# use 5.0.0
+
 python .openapi-generator/downloadHoneybeeSchema.py
 python .openapi-generator/pre_gen_script.py ".openapi-docs/model_inheritance.json"
 
-npx openapi-generator generate -i ".openapi-docs/simulation-parameter_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json  
-npx openapi-generator generate -i ".openapi-docs/model_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
+npx @openapitools/openapi-generator-cli generate -i ".openapi-docs/simulation-parameter_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json  
+npx @openapitools/openapi-generator-cli generate -i ".openapi-docs/model_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
 
 python .openapi-generator/post_gen_script.py ".openapi-docs/simulation-parameter_inheritance.json"
 python .openapi-generator/post_gen_script.py ".openapi-docs/model_inheritance.json"
