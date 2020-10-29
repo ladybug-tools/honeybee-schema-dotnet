1bd8c19cc3ed5bb3dcd6209c1cbf9f4f89c58510
MingboPeng <mingo1214@gmail.com>
2020-10-29 12:33:12 -0400

    chore(CI): add downloadHoneybeeSchema.py

---
+++ .openapi-generator/downloadHoneybeeSchema.py
@@ -0,0 +1,39 @@
+import os
+import urllib.request
+import json
+import shutil
+
+
+def download(url, dir):
+    json_url = urllib.request.urlopen(url)
+    data = json.loads(json_url.read())
+
+    json_file = os.path.basename(url)
+    save_path = os.path.join(dir, json_file)
+    print(f'Saving {json_file} to {save_path}')
+
+    with open(save_path, "w") as j:
+        json.dump(data, j, indent=4)
+
+
+saving_dir = os.path.join(os.getcwd(), '.openapi-docs')
+# clean up the folder
+if os.path.exists(saving_dir):
+    shutil.rmtree(saving_dir)
+os.mkdir(saving_dir)
+
+# downlaod model
+json_file = "https://www.ladybug.tools/honeybee-schema/model_inheritance.json"
+download(json_file, saving_dir)
+
+# download model mapper
+mapper_json = json_file.replace("inheritance.json", "mapper.json")
+download(mapper_json, saving_dir)
+
+# downlaod simulation-parameter
+json_file = "https://www.ladybug.tools/honeybee-schema/simulation-parameter_inheritance.json"
+download(json_file, saving_dir)
+
+# download simulation-parameter mapper
+mapper_json = json_file.replace("inheritance.json", "mapper.json")
+download(mapper_json, saving_dir)
--- .openapi-generator/generate_dev.sh
+++ .openapi-generator/generate_dev.sh
@@ -2,21 +2,16 @@
 # Install openapi-generator
 #   npm install @openapitools/openapi-generator-cli@cli-4.3.0
 
-
+python .openapi-generator/downloadHoneybeeSchema.py
 python .openapi-generator/pre_gen_script.py
-npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/simulation-parameter_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json  
-npx openapi-generator generate -i "https://www.ladybug.tools/honeybee-schema/model_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
-python .openapi-generator/post_gen_script.py "https://www.ladybug.tools/honeybee-schema/simulation-parameter_inheritance.json"
-python .openapi-generator/post_gen_script.py "https://www.ladybug.tools/honeybee-schema/model_inheritance.json"
-python .openapi-generator/update_assembly_version.py
-python .openapi-generator/create_interface.py "https://www.ladybug.tools/honeybee-schema/simulation-parameter_mapper.json"
-python .openapi-generator/create_interface.py "https://www.ladybug.tools/honeybee-schema/model_mapper.json"
-
 
 npx openapi-generator generate -i ".openapi-docs/simulation-parameter_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json  
 npx openapi-generator generate -i ".openapi-docs/model_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
+
 python .openapi-generator/post_gen_script.py ".openapi-docs/simulation-parameter_inheritance.json"
 python .openapi-generator/post_gen_script.py ".openapi-docs/model_inheritance.json"
+
 python .openapi-generator/update_assembly_version.py
+
 python .openapi-generator/create_interface.py ".openapi-docs/simulation-parameter_mapper.json"
 python .openapi-generator/create_interface.py ".openapi-docs/model_mapper.json"
