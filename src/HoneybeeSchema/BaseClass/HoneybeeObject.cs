96a5389af03ec74478f8ac498a39ff552eb672a7
MingboPeng <mingo1214@gmail.com>
2020-10-30 23:59:35 -0400

    fix(baseclass): fix base class with OpenAPIGenBaseModel

--- src/HoneybeeSchema/BaseClass/HoneybeeObject.cs
+++ src/HoneybeeSchema/BaseClass/HoneybeeObject.cs
@@ -12,14 +12,7 @@ namespace HoneybeeSchema
         {
         }
 
-        /// <summary>
-        /// Gets or Sets Type
-        /// The default value is set to "InvalidSchemaObject", which should be overridden in subclass' constructor.
-        /// </summary>
-        [DataMember(Name="type", EmitDefaultValue=false)]
-        [JsonProperty("type")]
-        public string Type { get; protected internal set; } = "InvalidSchemaObject";
-
+        
         public abstract string ToString(bool detailed);
 
         public abstract HoneybeeObject Duplicate();
