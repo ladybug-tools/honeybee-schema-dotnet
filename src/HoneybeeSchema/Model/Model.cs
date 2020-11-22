d3f7c26b3913bd20f0fdbde8c4a736074bf1de62
ladybugbot <ladybugbot@users.noreply.github.com>
2020-11-22 22:25:19 +0000

    Update Schema to v1.39.10

--- src/HoneybeeSchema/Model/Model.cs
+++ src/HoneybeeSchema/Model/Model.cs
@@ -43,7 +43,7 @@ namespace HoneybeeSchema
         { 
             // Set non-required readonly properties with defaultValue
             this.Type = "Model";
-            this.Version = "1.39.9";
+            this.Version = "1.39.10";
         }
         
         /// <summary>
@@ -80,7 +80,7 @@ namespace HoneybeeSchema
 
             // Set non-required readonly properties with defaultValue
             this.Type = "Model";
-            this.Version = "1.39.9";
+            this.Version = "1.39.10";
         }
 
         /// <summary>
