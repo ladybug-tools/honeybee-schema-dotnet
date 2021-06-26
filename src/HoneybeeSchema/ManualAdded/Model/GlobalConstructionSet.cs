da1c8acef9251248aa0c0c8e3161fd513874aec6
MingboPeng <mingo1214@gmail.com>
2021-06-26 16:10:10 -0400

    fix(Global_default): add create_global_default.py

---
+++ src/HoneybeeSchema/ManualAdded/Model/GlobalConstructionSet.cs
@@ -0,0 +1,24 @@
+﻿
+namespace HoneybeeSchema
+{
+    partial class GlobalConstructionSet
+	{
+		private static GlobalConstructionSet _default;
+
+		public static GlobalConstructionSet Default
+		{
+			get 
+			{
+				if (_default == null)
+				{
+					_default = GlobalConstructionSet.FromJson(_defaultString);
+				}
+
+				return _default; 
+			}
+		}
+
+
+		private static string _defaultString = Resource.GlobalConstructionSet;
+	}
+}
---
+++ src/HoneybeeSchema/ManualAdded/Model/GlobalMidifierSet.cs
@@ -0,0 +1,24 @@
+﻿
+namespace HoneybeeSchema
+{
+    partial class GlobalModifierSet
+	{
+		private static GlobalModifierSet _default;
+
+		public static GlobalModifierSet Default
+		{
+			get 
+			{
+				if (_default == null)
+				{
+					_default = GlobalModifierSet.FromJson(_defaultString);
+				}
+
+				return _default; 
+			}
+		}
+
+
+		private static string _defaultString = Resource.GlobalModifierSet;
+	}
+}
