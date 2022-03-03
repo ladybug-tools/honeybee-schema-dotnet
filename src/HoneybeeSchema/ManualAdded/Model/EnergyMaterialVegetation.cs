3f7bfc9cc832c13d3d87e4b697313684947eb754
MingboPeng <mingo1214@gmail.com>
2022-03-03 13:19:01 +0800

    fix(EnergyMaterialVegetation): fix with the new EnergyMaterialVegetation

---
+++ src/HoneybeeSchema/ManualAdded/Model/EnergyMaterialVegetation.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace HoneybeeSchema
+{
+    public partial class EnergyMaterialVegetation
+    {
+        public double RValue => this.Thickness / this.Conductivity;
+        public double UValue => this.Conductivity / this.Thickness;
+    }
+}
