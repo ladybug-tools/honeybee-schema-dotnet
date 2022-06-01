0ff5724adae611f75ef29a92e639fcddd55161c8
MingboPeng <mingo1214@gmail.com>
2022-06-01 18:57:22 +0800

    fix(Validation): add baseclass OpenAPIGenBaseModel

---
+++ src/HoneybeeSchema/ManualAdded/Model/Validation.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace HoneybeeSchema
+{
+    public partial class ValidationError : OpenAPIGenBaseModel
+    {
+    }
+
+    public partial class ValidationParent : OpenAPIGenBaseModel
+    {
+    }
+
+    public partial class ValidationReport : OpenAPIGenBaseModel
+    {
+    }
+}
