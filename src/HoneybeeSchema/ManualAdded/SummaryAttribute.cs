531ca0a23ba27fbe39aeb8b0a20d9446c42d8228
MingboPeng <mingo1214@gmail.com>
2022-10-20 20:43:11 -0400

    fix(SummaryAttribute): add helpers

--- src/HoneybeeSchema/ManualAdded/SummaryAttribute.cs
+++ src/HoneybeeSchema/ManualAdded/SummaryAttribute.cs
@@ -5,9 +5,29 @@ namespace HoneybeeSchema
     public class SummaryAttribute : Attribute
     {
         private string _summary;
+
+        public string Summary
+        {
+            get => _summary;
+            //set => _summary = value;
+        }
+
         public SummaryAttribute(string summary)
         {
             this._summary = summary;
+        }
+
+        public static string GetSummary(Type t)
+        {
+            var att = (SummaryAttribute)Attribute.GetCustomAttribute(t, typeof(SummaryAttribute));
+            return att?.Summary;
+        }
+
+        public static string GetSummary(Type t, string property)
+        {
+            var prop = t.GetProperty(property);
+            var att = (SummaryAttribute)Attribute.GetCustomAttribute(prop, typeof(SummaryAttribute));
+            return att?.Summary;
         }
     }
 }
