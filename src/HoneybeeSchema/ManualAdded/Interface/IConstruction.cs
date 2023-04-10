7ccae90e8b9844f6526313a04d5271f0437e2de6
MingboPeng <mingo1214@gmail.com>
2023-04-10 21:14:17 +0800

    fix(IConstruction): add IWindowConstruction, IOpaqueConstruction

--- src/HoneybeeSchema/ManualAdded/Interface/IConstruction.cs
+++ src/HoneybeeSchema/ManualAdded/Interface/IConstruction.cs
@@ -7,4 +7,29 @@ namespace HoneybeeSchema.Energy
 
 		IConstruction GenReversedConstruction();
 	}
+
+    public partial interface IOpaqueConstruction
+    {
+    }
+
+    public partial interface IWindowConstruction
+    {
+    }
+}
+
+namespace HoneybeeSchema
+{
+   
+    //public partial class AirBoundaryConstruction : HoneybeeSchema.Energy.IConstruction { }
+    //public partial class AirBoundaryConstructionAbridged : HoneybeeSchema.Energy.IConstruction { }
+    public partial class OpaqueConstruction : HoneybeeSchema.Energy.IOpaqueConstruction { }
+    public partial class OpaqueConstructionAbridged : HoneybeeSchema.Energy.IOpaqueConstruction { }
+    //public partial class ShadeConstruction : HoneybeeSchema.Energy.IConstruction { }
+
+    public partial class WindowConstruction : HoneybeeSchema.Energy.IWindowConstruction { }
+    public partial class WindowConstructionAbridged : HoneybeeSchema.Energy.IWindowConstruction { }
+    public partial class WindowConstructionDynamic : HoneybeeSchema.Energy.IWindowConstruction { }
+    public partial class WindowConstructionDynamicAbridged : HoneybeeSchema.Energy.IWindowConstruction { }
+    public partial class WindowConstructionShade : HoneybeeSchema.Energy.IWindowConstruction { }
+    public partial class WindowConstructionShadeAbridged : HoneybeeSchema.Energy.IWindowConstruction { }
 }
