bd087a3a6fa720505db7daf9201536a10eb97823
MingboPeng <mingo1214@gmail.com>
2023-05-03 19:00:56 -0400

    feat(HVACScenario): add HVACScenario

---
+++ src/HoneybeeSchema/ManualAdded/Model/HVACScenario.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System;
+
+namespace HoneybeeSchema
+{
+    [Summary(@"A design option of HVAC scenario that contains a group of HVAC detailed systems.")]
+    [Serializable]
+    [DataContract(Name = "HVACScenario")]
+    public class HVACScenario : IDdBaseModel, IEquatable<HVACScenario>, IValidatableObject
+    {
+        [JsonConstructorAttribute]
+        protected HVACScenario()
+        {
+            this.Type = "HVACScenario";
+        }
+
+        //public List<detailed> MyProperty { get; set; }
+    }
+}
