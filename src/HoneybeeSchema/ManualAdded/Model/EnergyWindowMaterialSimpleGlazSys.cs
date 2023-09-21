17452dc507b618ec6999258eb0ddcd0b29cca8ad
MingboPeng <mingo1214@gmail.com>
2023-09-21 17:10:26 +0800

    fix(EnergyWindowMaterialSimpleGlazSys): fix RValue calculation without air films

--- src/HoneybeeSchema/ManualAdded/Model/EnergyWindowMaterialSimpleGlazSys.cs
+++ src/HoneybeeSchema/ManualAdded/Model/EnergyWindowMaterialSimpleGlazSys.cs
@@ -4,16 +4,22 @@ namespace HoneybeeSchema
 {
     public partial class EnergyWindowMaterialSimpleGlazSys
     {
-        private const double _film_resistance = 1 / 23.0 + 1 / 8.0;
+        //https://github.com/ladybug-tools/honeybee-energy/blob/f76222320811116c01a773b9fda05e26c02da277/honeybee_energy/material/glazing.py#L616-L625
+        private double out_film_r => 1 / ((0.025342 * this.UFactor) + 29.163853);
+
+        private double in_film_r => this.UFactor < 5.85 ? 1 / ((0.359073 * Math.Log(UFactor)) + 6.949915) : 1 / ((1.788041 * this.UFactor) - 2.886625);
+
+
         public double Emissivity => 0.84;
-        public double RValue => (1 / this.UFactor) - _film_resistance;
+        public double RValue => (1 / this.UFactor) - out_film_r - in_film_r;
 
         public double UValue => 1 / this.RValue;
         public double RFactor => 1 / this.UFactor;
 
         public double SolarTransmittance => CalSolarTransmittance(this.UFactor, this.Shgc);
 
-        //https://github.com/ladybug-tools/honeybee-energy/blob/42ce9cffced5eb9c05a7e74183e244b40a326a98/honeybee_energy/material/glazing.py#L579
+
+        //https://github.com/ladybug-tools/honeybee-energy/blob/f76222320811116c01a773b9fda05e26c02da277/honeybee_energy/material/glazing.py#L628-L642
         public static double CalSolarTransmittance(double uFactor, double shgc)
         {
             var term_1 = 0.0;
@@ -36,7 +42,7 @@ namespace HoneybeeSchema
             {
                 return term_1;
             }
-            else if (uFactor < 3.4)
+            else if (uFactor <= 3.4)
             {
                 return term_2;
             }
