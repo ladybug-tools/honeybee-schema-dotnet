7e3852849341d4bf71e6e1a3f39c392354bfb388
MingboPeng <mingo1214@gmail.com>
2023-12-11 14:07:41 +0800

    fix(schema): update schema to 1.55

--- src/HoneybeeSchema/Interface/Energy/Ventcool.cs
+++ src/HoneybeeSchema/Interface/Energy/Ventcool.cs
@@ -10,6 +10,7 @@ namespace HoneybeeSchema
 {
 	public partial class AFNCrack: HoneybeeSchema.Energy.IVentcool {}
 	public partial class VentilationControlAbridged: HoneybeeSchema.Energy.IVentcool {}
+	public partial class VentilationFan: HoneybeeSchema.Energy.IVentcool {}
 	public partial class VentilationOpening: HoneybeeSchema.Energy.IVentcool {}
 	public partial class VentilationSimulationControl: HoneybeeSchema.Energy.IVentcool {}
 }
