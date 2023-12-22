fbdd0cb46deb9e6d1ba6ccefec621cb15947973e
MingboPeng <mingo1214@gmail.com>
2023-12-22 19:03:47 +0800

    fix(IDdEnergyBaseModel): support handling user data

--- src/HoneybeeSchema/ManualAdded/Interface/IDdBaseModel_Manual.cs
+++ src/HoneybeeSchema/ManualAdded/Interface/IDdBaseModel_Manual.cs
@@ -6,14 +6,9 @@ namespace HoneybeeSchema
         // string Identifier { get; set;}
         // string DisplayName { get; set; }
         object UserData { get; set; }
+        
     } 
 
-    
-}
-
-namespace HoneybeeSchema
-{
-     
     public partial class IDdBaseModel: IIDdBaseModel
     {
         /// <summary>
@@ -27,4 +22,20 @@ namespace HoneybeeSchema
         /// <param name="vaule"></param>
         public void AddData(string key, object vaule) => this.AddUserData(key, vaule);
     }
+
+       
+    public partial class IDdEnergyBaseModel: IIDdBaseModel
+    {
+            /// <summary>
+        /// Get UserData in Dictionary
+        /// </summary>
+        public System.Collections.Generic.Dictionary<string, object> UserDictionary => this.GetUserData();
+        /// <summary>
+        /// Add a new value to UserData
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="vaule"></param>
+        public void AddData(string key, object vaule) => this.AddUserData(key, vaule);
+    }
+    
 }
