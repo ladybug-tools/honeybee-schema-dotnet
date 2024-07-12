11ba682aeeb8f6cedb1a9520a4bf415682295070
Mingbo Peng <mingo1214@gmail.com>
2024-07-11 22:37:48 -0400

    feat(TypeScript): add generator and TypeScriptSDK

---
+++ .nswag-generator/Generator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NJsonSchema;
+using NJsonSchema.Generation;
+using NJsonSchema.Visitors;
+using NSwag;
+using NSwag.CodeGeneration;
+
+namespace SchemaGenerator;
+
+internal class Generator
+{
+    static string _sdkName = "ModelEditorSDK";
+    static string workingDir = Environment.CurrentDirectory;
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine($"Current working dir: {workingDir}");
+        Console.WriteLine(string.Join(",", args));
+
+        var rootDir = System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(workingDir));
+        var outputDir = System.IO.Path.Combine(rootDir, "Output");
+        System.IO.Directory.CreateDirectory(outputDir);
+
+        GenDTO.Execute();
+        //var f = @"C:\Users\mingo\Repos\pollination\honeybee-schema-dotnet\.nswag-generator\Output\HoneybeeSchemaSDK.ts";
+        //var tsCode = System.IO.File.ReadAllText(f);
+        //tsCode = Processor.ProcessTypeScript(tsCode);
+
+        //var aa = "";
+
+        // generate processors
+        //GenProcessor.Execute();
+    }
+
+}
