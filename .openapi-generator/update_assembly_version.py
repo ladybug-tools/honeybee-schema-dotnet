35ea386fce44f09b4fd134748ac8c277957d6d71
Mingbo Peng <mingo1214@gmail.com>
2024-07-24 21:39:48 -0400

    fix(generator): script to update versions

--- .openapi-generator/update_assembly_version.py
+++ .openapi-generator/update_assembly_version.py
@@ -30,25 +30,26 @@ with urllib.request.urlopen(api) as r:
             print(f'Schema version {schema_version} is newer than the latest version on Nuget: {latest_version}')
 
 
+
+print(f'Getting an existing version: {new_version}')
+
 # increment version
-version_digits = new_version.split('.')
+version_digits = re.split(r'[.-]', new_version)
+print(version_digits)
 
 
-print(len(version_digits))
 if len(version_digits) == 3:
-    new_version = f'{new_version}.1'
-elif version_digits[-1] != '0':
+    new_version = f'{new_version}-1'
+else:
     v = int(version_digits[-1]) + 1
     version_digits = version_digits[0:-1]
-    version_digits.append(str(v))
-    new_version = '.'.join(version_digits)
-
-
+    version = '.'.join(version_digits)
+    new_version = f'{version}-{v}'
 
 print(f'New version: {new_version}')
 
 
-# update the version
+# update the version for CSharp
 assembly_file = os.path.join(os.getcwd(), 'src', package_name, f'{package_name}.csproj')
 
 with open(assembly_file, encoding='utf-8', mode='r') as csFile:
@@ -56,4 +57,14 @@ with open(assembly_file, encoding='utf-8', mode='r') as csFile:
 with open(assembly_file, encoding='utf-8', mode='w') as f:
     s = re.sub(r"(?<=\SVersion\>)\S+(?=\<\/)", new_version, s)
     print(f"Update AssemblyVersion to: {new_version}")
+    f.write(s)
+
+# update the version for TypeScript
+ts_package_file = os.path.join(os.getcwd(), 'src', 'TypeScriptSDK', 'package.json')
+
+with open(ts_package_file, encoding='utf-8', mode='r') as file:
+    s = file.read()
+with open(ts_package_file, encoding='utf-8', mode='w') as f:
+    s = re.sub(r"(?<=version\": \")[^\"]+(?=\")", new_version, s)
+    print(f"Update package.json to: {new_version}")
     f.write(s)
