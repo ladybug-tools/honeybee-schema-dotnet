298baa7fc1afccc5c5ccc7ab2ecfaf7c6e0ecea3
Mingbo Peng <mingo1214@gmail.com>
2024-08-02 23:40:09 -0400

    fix(TypeScriptSDK): add generated files

--- src/TypeScriptSDK/models/index.ts
+++ src/TypeScriptSDK/models/index.ts
@@ -1,2 +1,8 @@
 
-export * from "./HoneybeeSchemaSDK";
+export * from "./_OpenAPIGenBaseModel";
+export * from "./Autocalculate";
+export * from "./BuildingTypes";
+export * from "./ClimateZones";
+export * from "./EfficiencyStandards";
+export * from "./Location";
+export * from "./ProjectInfo";
