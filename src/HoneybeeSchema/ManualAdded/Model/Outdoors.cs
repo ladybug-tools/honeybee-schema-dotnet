da6350c8361a172a4b760737148fc207ee16d65a
Mingbo Peng <mingo1214@gmail.com>
2024-08-11 23:12:04 -0400

    fix(Outdoor): remove redundant Outdoor constructor

--- src/HoneybeeSchema/ManualAdded/Model/Outdoors.cs
+++ src/HoneybeeSchema/ManualAdded/Model/Outdoors.cs
@@ -1,12 +1,12 @@
-﻿namespace HoneybeeSchema
-{
-    public partial class Outdoors
-    {
-        /// <summary>
-        /// Initialize a default Outdoor instance with default autocalculated view factor.
-        /// </summary>
-        public Outdoors():this(viewFactor: new Autocalculate())
-        {
-        }
-    }
-}
+﻿//namespace HoneybeeSchema
+//{
+//    public partial class Outdoors
+//    {
+//        /// <summary>
+//        /// Initialize a default Outdoor instance with default autocalculated view factor.
+//        /// </summary>
+//        public Outdoors():this(viewFactor: new Autocalculate())
+//        {
+//        }
+//    }
+//}
