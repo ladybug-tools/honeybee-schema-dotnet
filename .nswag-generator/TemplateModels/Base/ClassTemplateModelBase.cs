8cd3ebb01a6ff2aa0e2ddf1718786d84ce93523f
Mingbo Peng <mingo1214@gmail.com>
2024-08-21 22:36:02 -0400

    fix(generator): honer input class name

--- .nswag-generator/TemplateModels/Base/ClassTemplateModelBase.cs
+++ .nswag-generator/TemplateModels/Base/ClassTemplateModelBase.cs
@@ -20,9 +20,9 @@ public class ClassTemplateModelBase
         Description = json.Description?.Replace("\n", "\\n")?.Replace("\"", "\"\"");
         BaseDiscriminator = json.Discriminator;
 
-        ClassName = Helper.CleanName(json.Title);
+        ClassName = json.Title;
         Discriminator = ClassName;
         InheritedSchema = json.InheritedSchema;
-        Inheritance = Helper.CleanName(InheritedSchema?.Title);
+        Inheritance = InheritedSchema?.Title;
     }
 }
