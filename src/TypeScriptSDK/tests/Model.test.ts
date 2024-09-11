b3d5e9b0ca5b47213c210f643e824eb8e65ecbac
Mingbo Peng <mingo1214@gmail.com>
2024-09-11 07:47:57 -0400

    test(ts): test glass

--- src/TypeScriptSDK/tests/Model.test.ts
+++ src/TypeScriptSDK/tests/Model.test.ts
@@ -1,5 +1,5 @@
 import { plainToClass } from "class-transformer";
-import { Model, Face3D } from "../models";
+import { Model, Face3D, Glass } from "../models";
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -17,6 +17,9 @@ test('test model', () => {
   const model = Model.fromJS(json);
   expect(model.display_name).toBe("ShoeBox0.5");
   expect(model.validate()).resolves.toBe(true);
+
+
+  model.properties.radiance
 });
 
 
@@ -43,4 +46,27 @@ test('test face3D', () => {
 
 
 });
+
+test('test glass', () => {
+  const json = {
+    "modifier": {
+        "type": "Void"
+    },
+    "type": "Glass",
+    "identifier": "generic_interior_window_vis_0.88",
+    "r_transmissivity": 0.9584154328610596,
+    "g_transmissivity": 0.9584154328610596,
+    "b_transmissivity": 0.9584154328610596,
+    "refraction_index": null,
+    "dependencies": []
+  }
+  const loc = Glass.fromJS(json);
+  expect(loc.validate()).resolves.toBe(true);
+
+  console.log(typeof loc.modifier)
+
+  
+
+});
+
 
