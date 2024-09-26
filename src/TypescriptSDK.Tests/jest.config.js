144f690c3c3da68825198d9e15775029bab99f09
Mingbo Peng <mingo1214@gmail.com>
2024-09-27 00:02:20 +0800

    test(TS): fix project setup

--- src/TypescriptSDK.Tests/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: "node",
-  setupFiles: ['./src/jest.setup.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-};
